8d76ecf77e8ed375e042c556df0acf6693e933be
yemanebiruk <bir_yem@yahoo.com>
2018-10-24 23:25:03 +0300

    started the header

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ class App extends Component {
         <header className="flex-container">
           
           <div className="flex-items">
-              <i className="fa fa-globe fa-3x"><i/> 
-              <h1>World countries List<h1/>                        
-          <div/>
-          <p id="noOfCountries"  className="flex-items">Total number of countries <p/> 
+              <i className="fa fa-globe fa-3x"></i> 
+              <h1>World countries List</h1>                        
+          </div>
+          <p id="noOfCountries"  className="flex-items">Total number of countries </p> 
           <div className="flex-items">                    
                   <input id="startWord" type="radio" name="wordPositon" value="startWord"/>
-                  <label for="starWord"> Starts with<label/>
+                  <label for="starWord"> Starts with</label>
                   <input id="anyWord" type="radio" name="wordPositon" value="anyWord"/> 
-                  <label for="anyWord">Includes<label/>
-          <div/>
+                  <label for="anyWord">Includes</label>
+          </div>
           
           <input id="searchInput" type="text"  placeholder="type search" class="flex-items"/>
          
           <div/>
      
-      <header/>
-      <div/>
+      </header>
+      </div>
     );
   }
 }
