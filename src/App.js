4a3e685e56e69ec8ec078e2c6916121bd0addd76
yemanebiruk <bir_yem@yahoo.com>
2018-10-24 20:30:54 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ class App extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>World Countries</h1>
-        </header>
-      </div>
+        <header className="flex-container">
+          
+          <div className="flex-items">
+              <i className="fa fa-globe fa-3x"><i/> 
+              <h1>World countries List<h1/>                        
+          <div/>
+          <p id="noOfCountries"  className="flex-items">Total number of countries <p/> 
+          <div className="flex-items">                    
+                  <input id="startWord" type="radio" name="wordPositon" value="startWord"/>
+                  <label for="starWord"> Starts with<label/>
+                  <input id="anyWord" type="radio" name="wordPositon" value="anyWord"/> 
+                  <label for="anyWord">Includes<label/>
+          <div/>
+          
+          <input id="searchInput" type="text"  placeholder="type search" class="flex-items"/>
+         
+          <div/>
+     
+      <header/>
+      <div/>
     );
   }
 }
