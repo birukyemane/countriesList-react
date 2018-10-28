9b18bda2ccb97be13e7b23f28bd601e43b9f1b80
yemanebiruk <bir_yem@yahoo.com>
2018-10-28 22:28:03 +0200

    Updates

---
+++ precache-manifest.e1ced17f2df9ecdb54a7b9dae5577016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5b072e05a521c1dd62",
+    "url": "/countriesList-react/static/js/runtime~main.cc5b072e.js"
+  },
+  {
+    "revision": "2055e93bdcea3dbd3f64",
+    "url": "/countriesList-react/static/js/main.2055e93b.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/countriesList-react/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "2055e93bdcea3dbd3f64",
+    "url": "/countriesList-react/static/css/main.f34e1a36.chunk.css"
+  },
+  {
+    "revision": "9ed82b4d622a3a5bedaf1c108420823d",
+    "url": "/countriesList-react/index.html"
+  }
+];
